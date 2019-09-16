c1f0a8ebe8b7658562dcd4dec6f1f27f7acd506d
blackbeltdiaries <github email address>
2019-09-16 13:53:46 -0600

    created react app client in mood-app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
-    get :authentication, to: 'authentications#index'
+    get :user, to: 'users#index'
   end
 end
