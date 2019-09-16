4d55cb07dc55d064dbe7dc9cf58d433165696ef1
blackbeltdiaries <github email address>
2019-09-16 17:39:15 -0600

    added to config/application.rb

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,6 @@ class App extends React.Component {
     return (
       <div>
         <h1>Fitbit Mood App</h1>
-        { (this.props.userData.loading === false && this.props.userData.users.length !== 0) ? (
-            <div>
-              <span>Current User: {this.props.userData.users[0].name}</span>
-            </div>
-          ) : (
-            null
-          )
-        }
 
         <br/><br/>
 
@@ -39,3 +31,14 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { fetchUser })(App);
+
+/*
+{ (this.props.userData.loading === false && this.props.userData.users.length !== 0) ? (
+    <div>
+      <span>Current User: {this.props.userData.users[0].name}</span>
+    </div>
+  ) : (
+    null
+  )
+}
+*/
--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,9 @@ module MoodApp
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.insert_after(ActiveRecord::QueryCache, ActionDispatch::Cookies)
+     config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
+
     config.api_only = true
   end
 end
